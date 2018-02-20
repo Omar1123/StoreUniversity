f56c8b098cbb3219bea093a03a04707a02d8c24a
Omar1123 <omar1123veliz@gmail.com>
2018-02-19 19:19:25 -0600

    ADD: Project settings

---
+++ src/Handler/java.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Handler;
+
+/**
+ *
+ * @author jake
+ */
+public class java {
+    
+    public void showOptions() {
+        System.out.println("Usuario");
+        System.out.println("1) Realizar compra");
+        System.out.println("2) Consultar saldo");
+        System.out.println("Administrador");
+        System.out.println("3) Corte de caja");
+    }
+    
+    
+    
+}
